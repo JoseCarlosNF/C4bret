96d972d6edf7e3929e47c5f09e90d857b0fed35c
JoseCarlosNF <josecarlos.n.f99@gmail.com>
2021-06-14 01:30:55 -0300

    refactor(src/regratransicao.mjs): muda o nome das chaves relacionadas a leitura e escrita na pilha

--- src/RegraTransicao.mjs
+++ src/RegraTransicao.mjs
@@ -2,14 +2,14 @@ export default class RegraTransicao {
   constructor([
     estado_origem,
     simbolo_lido_palavra,
-    simbolo_consumido_pilha,
+    simbolo_lido_pilha,
     estado_destino,
-    simbolo_inserido_pilha
+    simbolo_escrito_pilha
   ]) {
     this.estado_origem = estado_origem;
     this.simbolo_lido_palavra = simbolo_lido_palavra;
-    this.simbolo_consumido_pilha = simbolo_consumido_pilha;
+    this.simbolo_lido_pilha = simbolo_lido_pilha;
     this.estado_destino = estado_destino;
-    this.simbolo_inserido_pilha = simbolo_inserido_pilha;
+    this.simbolo_escrito_pilha = simbolo_escrito_pilha;
   }
 }
