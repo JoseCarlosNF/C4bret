28e94b9ef6ec852ddd41ab2b002e4967c5de32ff
JoseCarlosNF <josecarlos.n.f99@gmail.com>
2021-06-14 01:33:19 -0300

    feat(src/automato.mjs): adicionar método para processamento de palavras
    Infelizmente esse método ainda não é capaz de processar automatos com movimentos vazios

--- src/Automato.mjs
+++ src/Automato.mjs
@@ -1,6 +1,3 @@
-// import Estado from './Estado.js';
-// import Transicao from "./RegraTransicao.js";
-
 export default class Automato {
   constructor({
     alfabeto,
@@ -8,13 +5,51 @@ export default class Automato {
     funcao_transicao,
     estado_inicial,
     estados_finais,
-    pilha,
+    alfabeto_pilha,
   }) {
     this.alfabeto = alfabeto;
     this.estados = estados;
     this.funcao_transicao = funcao_transicao;
     this.estado_inicial = estado_inicial;
     this.estados_finais = estados_finais;
-    this.pilha = pilha;
+    this.alfabeto_pilha = alfabeto_pilha;
+    this.pilha = [];
+  }
+
+  processaPalavra(regras_transicao, palavra) {
+    let aceita = false;
+    palavra += " ";
+    let estado_atual = this.estado_inicial;
+
+    for (const simbolo_atual of palavra) {
+      for (const regra of regras_transicao) {
+        if ((estado_atual == regra.estado_origem) && (simbolo_atual == regra.simbolo_lido_palavra || regra.simbolo_lido_palavra == "?" || regra.simbolo_lido_palavra == "-")) {
+          if (this.#testePilhaVazia(regra.simbolo_lido_pilha, simbolo_atual)){
+            aceita = true;
+          }
+          else if ((this.pilha.length > 0) && (regra.simbolo_lido_pilha == this.pilha[this.pilha.length - 1])){
+            this.pilha.pop();
+          }
+          else if (regra.simbolo_escrito_pilha != "-")
+            this.pilha.push(regra.simbolo_escrito_pilha);
+          this.#imprimirProcessamento(regra);
+          estado_atual = regra.estado_destino;
+          break;
+        }
+      }
+    }
+    return aceita;
+  }
+
+  #imprimirProcessamento(regra) {
+    console.log(
+      `(${regra.estado_origem}, ${regra.simbolo_lido_palavra}, ${regra.simbolo_lido_pilha}) = {(${regra.estado_destino}, ${regra.simbolo_escrito_pilha}}) - Pilha: ${this.pilha}`
+    );
+  }
+
+  #testePilhaVazia(simbolo_lido_pilha, simbolo_atual) {
+    if ( this.pilha.length == 0 && simbolo_lido_pilha == "?" && simbolo_atual == " ")
+      return true;
+    return false;
   }
 }
